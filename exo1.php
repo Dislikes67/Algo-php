daf68f922f0a3658ec132376f3469c07a265184b
Mathias Rudolf <matt67creativ@hotmail.fr>
2024-06-06 11:21:21 +0200

    Orga exo / exo 7

--- exo1.php
+++ exo1.php
@@ -58,15 +58,19 @@ echo $phrase;
 }
 ?>
 
-<h1>Exercice 5<h1>
+<h1>Exercice 5</h1>
 
 <p>Ecrire un algorithme qui déclare une valeur en francs et qui la convertit en euros (arrondie à 2 décimales).<p>
 
 <?php
-$number = 34560.45
-$prixeuro = $number * 6,55957;
+$number = 34560.45;
+$total = $number / 6.55957;
+$roundNumber = round($total, 2);
 
-echo "Le prix en euros est de $prixeuro";
+
+echo "Le prix en euros est de $roundNumber €";
+
+
 
 
 ?>
