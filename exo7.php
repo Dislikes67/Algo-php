8199360a1956012eb6c5226a99b1024a3ae29e40
Mathias Rudolf <matt67creativ@hotmail.fr>
2024-06-06 12:05:32 +0200

    Exo 7 à jour

--- exo7.php
+++ exo7.php
@@ -4,9 +4,25 @@
 
 <?php
 //poussin entre 6 et 7 ans ; Pupille entre 8 et 9 ans ; Minime entre 10 et 11 ans ; Cadet à partir de 12 ans
-$poussin = Poussin;
-$pupille = Pupille;
-$minime = Minime;
-$cadet = Cadet;
- 
-if  >= $b
+$age = 10;
+
+if ($age>=12) {
+    
+    echo "cadet";
+}
+elseif ($age<=12 && $age>=9) {
+
+    echo "Minime";
+}
+elseif ($age<=10 && $age>=7) {
+
+    echo "Pupille";
+}
+elseif ($age<=8 && $age>=5) {
+
+    echo "Poussin";
+}
+else {
+    echo "Non pris en charge";
+}
+?>    
