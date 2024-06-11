50ffc7d4896b4135e1381063da1633b1b63b8559
Mathias Rudolf <matt67creativ@hotmail.fr>
2024-06-11 16:50:11 +0200

    Exo 11

---
+++ exo11.php
@@ -0,0 +1,33 @@
+<h1>Exercice 11</h1>
+
+<p>Initialiser un tableau de x marques de voitures. Ecrire un algorithme permettant de parcourir ce tableau et d'en afficher le contenu (une marque par ligne). Il est également demandé d'afficher le nombre de marque de voiture présentes dans le tableau.</p>
+
+<?php
+
+$marques = ["Peugeot", "Renault", "BMW", "Mercedes"];
+
+$nbmarques = count($marques);
+
+echo "Il y a $nbmarques marques de voitures dans le tableau :<br>";
+echo $marques[0]. ' <br>' . $marques[1]. ' <br>' . $marques[2]. ' <br>' . $marques[3];
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
