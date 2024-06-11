06cd35478436d80f98e512680a6e346034fec9be
Mathias Rudolf <matt67creativ@hotmail.fr>
2024-06-11 16:00:51 +0200

    Fin exo 10

--- exo10.php
+++ exo10.php
@@ -1,6 +1,6 @@
 <h1>Exercice 10</h1>
 
-<p>A partir d'un montant à payer et d'une somme versée pour régler un achat, écrire l'algorithme qui affiche à un utilisateur un rendu de monnaie en nombre de billets de 10€ et 5€, de pièces de 2€ et 1€.
+<p>A partir d'un montant à payer et d'une somme versée pour régler un achat, écrire l'algorithme qui affiche à un utilisateur un rendu de monnaie en nombre de billets de 10€ et 5€, de pièces de 2€ et 1€.</p>
 
 
 <?php
@@ -10,18 +10,23 @@ $montantaPayer = 152;
 $montantVerse = 200;
 $rendu = 48;
 
-$dix= intdiv($rendu,10);
+$dix = intdiv($rendu,10);           // Est ce qu'il y a un moyen de directement mettre le texte dans la variable ?
 $reste = ($rendu % 10);
 
 $cinq = intdiv($reste,5);
-$reste2 = intdiv($r);
-$deux = 0;
-$un = 0;
+$reste2 = ($reste % 5);
 
-echo "$dix";
+$deux = intdiv($reste2,2);
+$reste3 = ($reste2 % 2);
 
+$un = intdiv($reste3,1);
+$reste4 =($reste3 % 1);
 
-
+echo "Montant à payer : $montantaPayer<br>";
+echo "Montant versé : $montantVerse<br>";
+echo "Reste à payer : $rendu<br>";
+echo "*****************************************<br>";
+echo "Rendu de monnaie : <br> $dix billets de 10€  - $cinq billet de 5€ - $deux pièce de 2€ - $un pièce de 1€";
 
 
 
