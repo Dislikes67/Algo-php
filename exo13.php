8bb694f4d1ed3dce89c33677f440f07df2b3fa8d
Mathias Rudolf <matt67creativ@hotmail.fr>
2024-06-12 12:17:03 +0200

    exo13

---
+++ exo13.php
@@ -0,0 +1,14 @@
+<h1>Exercice 13</h1>
+
+<p>Calculer la moyenne générale d'un élève dont les notes sont renseignées dans un tableau (2 décimales).</p>
+
+<?php
+
+$notes = [10,12,8,19,3,16,11,13,9];
+
+$somme = array_sum($notes);
+$moyenne = $somme / count($notes);
+$arrondi = round($moyenne, 2);
+
+echo "Les notes obtenus par l'élève sont : print_r($notes)";
+?> 
